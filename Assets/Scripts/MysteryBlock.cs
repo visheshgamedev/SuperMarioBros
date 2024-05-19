e084d0b5d701c421961a845d9308180056eccfdf
visheshgamedev <168469856+visheshgamedev@users.noreply.github.com>
2024-05-19 16:42:38 +0530

    Changes in mystery box
    Added trigger to bottom of mystery box

--- Assets/Scripts/MysteryBlock.cs
+++ Assets/Scripts/MysteryBlock.cs
@@ -15,7 +15,7 @@ public class MysteryBlock : MonoBehaviour
         boxCollider = GetComponent<BoxCollider2D>();
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player") && this.enabled == true)
         {
